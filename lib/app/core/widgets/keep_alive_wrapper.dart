7f24fb66f44953b52435f92f1ab9b8a2900a59cd
Omer Gamliel <omergamliel3@gmail.com>
2020-10-10 00:12:02 +0300

    add keep alive wrapper widget

---
+++ lib/app/core/widgets/keep_alive_wrapper.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+class KeepAliveWrapper extends StatefulWidget {
+  final Widget child;
+  KeepAliveWrapper({@required this.child});
+  @override
+  _KeepAliveWrapperState createState() => _KeepAliveWrapperState();
+}
+
+class _KeepAliveWrapperState extends State<KeepAliveWrapper>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Container(child: widget.child);
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
