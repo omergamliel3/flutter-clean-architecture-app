d2369d54bdab64df57fc40e61c3c27e03d256ec4
Omer Gamliel <omergamliel3@gmail.com>
2020-10-11 01:00:02 +0300

    update home_get folder

--- lib/app/presentation/index.dart
+++ lib/app/presentation/index.dart
@@ -1,4 +1,4 @@
 export '../presentation/loading/loading_view.dart';
 export '../presentation/loading/loading_binding.dart';
-export '../presentation/home/home_view.dart';
-export '../presentation/home/home_binding.dart';
+export '../presentation/home_get/home_view.dart';
+export '../presentation/home_get/home_binding.dart';
