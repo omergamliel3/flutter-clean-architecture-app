4ee05c8b1d68d3e9143d20894c5010cbba30576f
Omer Gamliel <omergamliel3@gmail.com>
2020-11-04 02:06:23 +0200

    getRemoteArticles usecase test

---
+++ test/domain/usecases/get_remote_articles_test.dart
@@ -0,0 +1,52 @@
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:dartz/dartz.dart';
+
+import 'package:getx_hacker_news_api/app/core/errors/failure.dart';
+
+import 'package:getx_hacker_news_api/app/domain/usecases/get_remote_articles.dart';
+import 'package:getx_hacker_news_api/app/domain/repositories/articles_repository.dart';
+
+import '../../test_helper.dart';
+
+class ArticlesRepositoryMock extends Mock implements ArticlesRepository {}
+
+void main() {
+  GetRemoteArticles usecase;
+  ArticlesRepository articlesRepositoryMock;
+
+  setUp(() {
+    articlesRepositoryMock = ArticlesRepositoryMock();
+    usecase = GetRemoteArticles(articlesRepositoryMock);
+  });
+
+  test('should get remote articles from the repository', () async {
+    // arrange
+    when(articlesRepositoryMock.getRemoteArticles())
+        .thenAnswer((realInvocation) => Future.value(Right(articles)));
+
+    // act
+    final result = await usecase.call();
+
+    // assert
+    expect(result, Right(articles));
+    verify(articlesRepositoryMock.getRemoteArticles());
+    verifyNoMoreInteractions(ArticlesRepositoryMock());
+  });
+
+  test('should get failure from the repository', () async {
+    const failure = Failure('something went wrong');
+
+    // arrange
+    when(articlesRepositoryMock.getRemoteArticles())
+        .thenAnswer((realInvocation) => Future.value(const Left(failure)));
+
+    // act
+    final result = await usecase.call();
+
+    // assert
+    expect(result, const Left(failure));
+    verify(articlesRepositoryMock.getRemoteArticles());
+    verifyNoMoreInteractions(ArticlesRepositoryMock());
+  });
+}
