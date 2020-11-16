16b13ae64d59390ffad7b695c8579ed597f5457f
Omer Gamliel <omergamliel3@gmail.com>
2020-11-16 13:11:55 +0200

    remove unnecessary comments

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -3,14 +3,10 @@ import '../presentation/index.dart';
 import 'package:get/get.dart';
 part 'app_routes.dart';
 
-/// ! note: you can change between HomeViewBloc, and HomeViewGetx.
-/// ! Those are same implementation with different state managment
-
 class AppPages {
   static const INITIAL = Routes.LOADING;
 
   static final routes = [
-    /// ! Bloc state managment, only use if registered ArticlesBloc in injector
     GetPage(
       name: Routes.HOME,
       page: () => HomeViewCubit(),
@@ -22,10 +18,3 @@ class AppPages {
     ),
   ];
 }
-
-///! GetX state managment option, replace with HomeViewBloc
-// GetPage(
-//   name: Routes.HOME,
-//   page: () => HomeViewGetX(),
-//   binding: HomeBinding(),
-// ),
